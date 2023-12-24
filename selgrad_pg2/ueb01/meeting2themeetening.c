f6160758df8421c9fc85f9a801085c124f626e69
Thibaut Endress <thibaut.endress27@proton.me>
2023-12-24 16:29:54 +0100

    now also with linked lists

--- selgrad_pg2/ueb01/meeting2themeetening.c
+++ selgrad_pg2/ueb01/meeting2themeetening.c
@@ -4,30 +4,40 @@
 #include <assert.h>
 #include <stdlib.h>
 
-typedef struct {
+typedef struct name {
     char *vorname, *nachname, anrede;
 } name;
+
+typedef struct node {
+    struct node *next;
+    name *data;
+}node;
 
 int MyStrLen(const char *name);
 char* MyStringSplit(const char *input, int startIndex, int endIndex);
 int FindChar(const char *input, char c);
-void teilnehmer(name *nameArr, int names);
+void teilnehmer(name *name);
 
 int main(int argc, char **argv)
 {
     if (argc > 1)
     {
+        /*
+        //  version using array
         name *nameArr = (name*) malloc(sizeof(name) * (argc-1));
         assert(nameArr != NULL);
         for (int i = 1; i < argc; i++)
         {
             int nameLen = MyStrLen(argv[i]);
             nameArr[i-1].vorname = MyStringSplit(argv[i], 0, FindChar(argv[i], ' ')-1);
-            nameArr[i-1].nachname = MyStringSplit(argv[i], FindChar(argv[i], ' ')+1, nameLen-3);  // -4 for \0, */m/f and ' '
+            nameArr[i-1].nachname = MyStringSplit(argv[i], FindChar(argv[i], ' ')+1, nameLen-3);  // -4 for \0, m/f/* and ' '
             nameArr[i-1].anrede = argv[i][nameLen-1];
         }
 
-        teilnehmer(nameArr, argc-1);
+        for (int i = 0; i < argc-1; i++)
+        {
+            teilnehmer(nameArr[i]);
+        }
 
         for (int i = 0; i < argc-1; i++)
         {
@@ -35,6 +45,40 @@ int main(int argc, char **argv)
             free(nameArr[i].nachname);
         }
         free(nameArr);
+        */
+
+        //  version using linked list
+        node *names = NULL;
+
+        for (int i = 1; i < argc; i++)
+        {
+            node *newNode = (node*) malloc(sizeof(node));
+            newNode->data = (name*) malloc(sizeof(name));
+            int nameLen = MyStrLen(argv[i]);
+            newNode->data->vorname = MyStringSplit(argv[i], 0, FindChar(argv[i], ' ')-1);
+            newNode->data->nachname = MyStringSplit(argv[i], FindChar(argv[i], ' ')+1, nameLen-3);
+            newNode->data->anrede = argv[i][nameLen-1];
+            newNode->next = names;
+            names = newNode;
+        }
+
+        node *head = names;
+        for (node *run = names; run; run = run->next)
+        {
+            teilnehmer(run->data);
+        }
+
+        names = head;
+        while(names != NULL)
+        {
+            node *tmp = names->next;
+            free(names->data->vorname);
+            free(names->data->nachname);
+            free(names->data);
+            free(names);
+            names = tmp;
+        }
+
     }
     return 0;
 }
@@ -77,22 +121,18 @@ int FindChar(const char *input, char c)
 }
 
 // prints first names of each name struct in passed array
-void teilnehmer(name *nameArr, int names)
+void teilnehmer(name *name)
 {
-    assert(nameArr != NULL);
-    for (int i = 0; i < names; i++)
+    switch(name->anrede)
     {
-        switch(nameArr[i].anrede)
-        {
-            case 'm':
-                printf("Hallo Herr %s\n", nameArr[i].nachname);
-                break;
-            case 'f':
-                printf("Hallo Frau %s\n", nameArr[i].nachname);
-                break;
-            case '*':
-                printf("Hallo %s\n", nameArr[i].vorname);
-                break;
-        }
+        case 'm':
+            printf("Hallo Herr %s\n", name->nachname);
+            break;
+        case 'f':
+            printf("Hallo Frau %s\n", name->nachname);
+            break;
+        case '*':
+            printf("Hallo %s\n", name->vorname);
+            break;
     }
 }
